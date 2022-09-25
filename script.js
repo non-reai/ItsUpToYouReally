3a431443770b9e29baefd64dd15a2443e3c19f34
nonreai <63729314+non-reai@users.noreply.github.com>
2022-09-25 23:52:54 +0000

    adf

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ var http = new XMLHttpRequest
 
 http.open("GET","https://getpantry.cloud/apiv1/pantry/259da317-fbba-4b87-afda-68171f60a086/basket/json")
 
+var currentRoomId = 0
+
 http.onload = function() {
   let result = JSON.parse(this.responseText)
+  getPosts(0,result)
   for (let i = 0; i < result["Rooms"].length; i++) {
     let b = document.createElement("button")
     b.innerText = result["Rooms"][i]["RoomName"]
     document.getElementById("rooms").appendChild(b)
     b.onclick = function () {
-      getPosts(result["Posts"][i]["PostRoomId"],result)
+      getPosts(result["Rooms"][i]["RoomId"],result)
     }
   }
 }
 
-http.send();
+http.send()
 
 function getPosts(roomId,result) {
+  currentRoomId = roomId
   while (document.getElementById("chat").firstChild) {
     document.getElementById("chat").firstChild.remove()
   }
@@ -26,11 +30,34 @@ function getPosts(roomId,result) {
       roomIdPosts.push(result["Posts"][i])
     }
   }
-  console.log(roomIdPosts)
   for (let i = 0; i < roomIdPosts.length; i++) {
+    let div = document.createElement("div")
+    document.getElementById("chat").appendChild(div)
     let text = document.createElement("p")
     text.innerText = roomIdPosts[roomIdPosts.length-i-1]["PostContent"]
-    text.classList.add("post")
-    document.getElementById("chat").appendChild(text)
+    div.classList.add("post")
+    div.appendChild(text)
   }
+}
+document.getElementById("postMessage").onclick = function () {
+  post()
+}
+function post() {
+  http.open("GET","https://getpantry.cloud/apiv1/pantry/259da317-fbba-4b87-afda-68171f60a086/basket/json")
+  http.onload = function() {
+    result = JSON.parse(this.responseText)
+    result["Posts"].push(
+      {
+        "PostRoomId": currentRoomId,
+        "PostContent": document.getElementById("textMessage").value
+      },
+    )
+    console.log(JSON.stringify(result))
+    http.setRequestHeader("Content-Type", "application/json");
+    http.open("POST","https://getpantry.cloud/apiv1/pantry/259da317-fbba-4b87-afda-68171f60a086/basket/json")
+    http.send(JSON.stringify(result))
+  }
+  http.send()
+
+
 }
